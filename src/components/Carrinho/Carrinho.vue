d4b3b556f30464fa3ff45a51cc1c9680b04cca8f
Eduardo Viega <eduardoviega12@gmail.com>
2022-11-03 02:15:42 -0300

    Produtos Carrinho

--- src/components/Carrinho/Carrinho.vue
+++ src/components/Carrinho/Carrinho.vue
@@ -67,11 +67,6 @@ export default {
             return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valor)
         },
     },
-    watch: {
-        produtosCarrinho(){
-            this.produtosCarrinho = this.produtosCarrinho
-        }
-    },
     components: { ItemCarrinho },
 }
 </script>
